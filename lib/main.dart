c384db9097bcc4c44fe5cd52e4cd66b5fc714716
kokoriru <yuka@ryukalice.com>
2021-11-17 14:00:37 +0900

    表示内容を info から hello の訳に変更

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            Text(l10n.info),
+            Text(l10n.hello),
           ],
         ),
       ),
